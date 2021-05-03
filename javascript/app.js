1fdbc6e5ad9d9d69cab1153f99156c087fe37ff4
tahmid1406 <tahmidpranto1406@gmail.com>
2021-05-03 09:22:58 +0600

    random generator bug fixed

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,104 @@
 
 
-// random number for question
+// variables for quiz question innerHTML setter
 
-var question_no = Math.floor(Math.random() * 5);
+const questionNo = document.querySelector('.question-no');
+const qText = document.querySelector('.question');
 
-// random option array for answers
-var loop = true;
 
-var random_order = [6,6,6,6];
+// variables for counting remaining, already done and current question
 
-var found_count = 0;
-while(loop){
-	if(found_count == 4) break;
-	var random_option = Math.floor(Math.random() * 4);
-	if(random_order.indexOf(random_option) == -1){
-		random_order[found_count] = random_option;
-		found_count++;
-	}else{
-		continue;
+
+let quesCounter = 0;
+let currentQues ;
+let stillAvailable = [];
+
+
+
+// // random option array for answers
+// var loop = true;
+
+// var random_order = [6,6,6,6];
+
+// var found_count = 0;
+// while(loop){
+// 	if(found_count == 4) break;
+// 	var random_option = Math.floor(Math.random() * 4);
+// 	if(random_order.indexOf(random_option) == -1){
+// 		random_order[found_count] = random_option;
+// 		found_count++;
+// 	}else{
+// 		continue;
+// 	}
+// }
+
+// console.log(random_order)
+
+
+
+function setAvailableQues(){
+	const qNo = questions.length;
+
+	for(let i=0; i<qNo; i++){
+		//stillAvailable.push(questions[i]);
+		stillAvailable[i] = true;
 	}
 }
 
-console.log(random_order)
+// function uniqueRandom(){
+// 	const randomQIndex = Math.abs(Math.floor(Math.random() * 5));
+// 	return randomQIndex;
+
+// 	if(stillAvailable[randomQIndex] === 6){
+// 		stillAvailable[randomQIndex] = 0;
+// 		console.log(randomQIndex);
+// 		return randomQIndex;
+// 	}else{
+// 		uniqueRandom();
+// 	}
+
+// }
+
+function setNewRandomQues(){
+	// 1. Change the question quesCounter
+	// 2. Generate new random ques and update html
+	// 3. make the existance of available question index
+
+	questionNo.innerHTML = "Question " + (quesCounter + 1);
+	quesCounter++;
+
+	var uniqueFinder = true;
+	while(uniqueFinder){
+		const ind = Math.abs(Math.floor(Math.random() * 5));
+		if(stillAvailable[ind] === true){
+			qText.innerHTML = questions[ind];
+			stillAvailable[ind] = false;
+			uniqueFinder = false;	
+			
+			console.log(ind);
+			console.log(stillAvailable);	
+		}
+	}
+}
+
+
+
+function nextQues(){
+	if(quesCounter === questions.length){
+		console.log("Quiz ends");
+	}else{
+		setNewRandomQues();
+	}
+}
+
+
+window.onload = function(){
+
+	// 1. push all questions to available question
+	// 2. set a new question (available, random) to the game area
+	
+	setAvailableQues();
+
+	setNewRandomQues();
+
+}
--- javascript/question.js
+++ javascript/question.js
@@ -10,29 +10,29 @@ const questions = [
 
 
 
-const options = [
-	{
-		op : ["option1 q1", "option2 q1", "option3 q1", "option4 q1"],
-		answer : 0
-	},
+// const options = [
+// 	{
+// 		op : ["option1 q1", "option2 q1", "option3 q1", "option4 q1"],
+// 		answer : 0
+// 	},
 
-	{
-		op : ["option1 q1", "option2 q1", "option3 q1", "option4 q1"],
-		answer : 1
-	},
+// 	{
+// 		op : ["option1 q2", "option2 q2", "option3 q2", "option4 q2"],
+// 		answer : 1
+// 	},
 
-	{
-		op : ["option1 q1", "option2 q1", "option3 q1", "option4 q1"],
-		answer : 2
-	},
+// 	{
+// 		op : ["option1 q3", "option2 q3", "option3 q3", "option4 q3"],
+// 		answer : 2
+// 	},
 
-	{
-		op : ["option1 q1", "option2 q1", "option3 q1", "option4 q1"],
-		answer : 3
-	},
+// 	{
+// 		op : ["option1 q4", "option2 q4", "option3 q4", "option4 q4"],
+// 		answer : 3
+// 	},
 
-	{
-		op : ["option1 q1", "option2 q1", "option3 q1", "option4 q1"],
-		answer : 4
-	}
-];
+// 	{
+// 		op : ["option1 q5", "option2 q5", "option3 q5", "option4 q5"],
+// 		answer : 4
+// 	}
+// ];
